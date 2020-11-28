b0982af84aa3b39e1bcb51fef72e380bb8a7538c
Ismail Zine El Abidine <55003503+samacyc@users.noreply.github.com>
2020-11-28 14:24:00 +0100

    cv api

---
+++ cv_api/face_detector/urls.py
@@ -0,0 +1,30 @@
+"""cv_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from .views import detect
+from django.urls import path 
+
+
+urlpatterns = [
+    
+    path('face_detection/detect/', detect )
+]
+"""urlpatterns = patterns('',
+    # Examples:
+    url(r'^face_detection/detect/$', 'face_detector.views.detect'),
+    # url(r'^$', 'cv_api.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)"""
